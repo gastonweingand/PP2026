55234c55463c4137aff55415a87b24493699237f
gaston <gaston@GASTONMACPRO>
2026-07-13 19:18:32 -0300

    Servicios de criptografía

---
+++ Services/Facade/CryptographyService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.Logic.Infraestructure;
+
+namespace Services.Facade
+{
+    public static class CryptographyService
+    {
+
+        public static string HashMd5(string textPlain)
+        {
+            return CryptographyLogic.HashMd5(textPlain);
+        }
+
+        public static string Encrypt(string clearText)
+        {
+            return CryptographyLogic.Encrypt(clearText);
+        }
+
+        public static string Decrypt(string clearText) {
+            return CryptographyLogic.Decrypt(clearText);
+
+        }
+    }
+}
